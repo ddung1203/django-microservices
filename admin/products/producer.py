27a8bd57116df8dc840a73d374791a92714525f6
Joongseok Jeon <jeonjungseok1203@gmail.com>
2023-05-17 17:25:33 +0000

    [FIX] closing AMQP connection

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,11 +1,10 @@
 import pika, json
 
-params = pika.URLParameters('amqps://tdetwkla:LN5DfpbG_YOvHSdSK8PXIjiE4D6aT0_X@dingo.rmq.cloudamqp.com/tdetwkla')
-
-connection = pika.BlockingConnection(params)
-
-channel = connection.channel()
+params = pika.URLParameters('amqp://192.168.56.100:5672')
 
 def publish(method, body):
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
     properties = pika.BasicProperties(method)
     channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
+    connection.close()
