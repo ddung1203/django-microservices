94bc2c3dcf16a4e7fb879c6fdfa8d536223f76f7
Joongseok Jeon <jeonjungseok1203@gmail.com>
2023-05-22 06:20:20 +0000

    [ADD] Django flask MSA

--- main/producer.py
+++ main/producer.py
@@ -1,6 +1,6 @@
 import pika, json
 
-params = pika.URLParameters('amqp://rabbitmq.default.svc.cluster.local:5672')
+params = pika.URLParameters('amqp://rabbitmq-svc.default.svc.cluster.local:5672')
 
 def publish(method, body):
     connection = pika.BlockingConnection(params)
